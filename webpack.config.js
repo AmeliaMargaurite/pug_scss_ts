b4b07c4036042e80d0486b7d7a487ef2ade2bf2e
AmeliaMargaurite <53086735+AmeliaMargaurite@users.noreply.github.com>
2023-02-03 16:10:07 +0100

    added functionality to convert pug to php. contact form now php based, error handling and displaying

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,19 @@ const alterPugFolderStructure = (pathData) => {
 	if (name === "index") {
 		return `${name}.html`;
 	}
+
+	const phpFile = name.split(".php");
+
+	if (phpFile.length > 1) {
+		if (phpFile[0] === "index") {
+			return `${phpFile[0]}.php`;
+		} else {
+			return nestedDir
+				? `${nestedDir}/${phpFile[0]}/index.php`
+				: `${phpFile[0]}/index.php`;
+		}
+	}
+
 	return nestedDir ? `${nestedDir}/${name}/index.html` : `${name}/index.html`;
 };
 
@@ -23,7 +36,7 @@ module.exports = {
 	entry: {
 		index: "./src/pages/index.pug",
 		about: "./src/pages/about.pug",
-		contact: "./src/pages/contact.pug",
+		contact: "./src/pages/contact.php.pug",
 		confirmation: "./src/pages/confirmation.pug",
 		gallery: "./src/pages/gallery.pug",
 		components: "./src/pages/components.pug",
